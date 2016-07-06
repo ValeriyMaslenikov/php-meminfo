771f9478ddee6973ce8cd0ec158705aef13b5ffc
Benoit Jacquemont <benoit@akeneo.com>
2016-07-06 11:38:41 +0200

    Initialize extension separation for PHP5 and PHP7

--- extension/php5/meminfo.c
+++ extension/php5/meminfo.c
@@ -4,6 +4,7 @@
 
 #include "php.h"
 #include "php_meminfo.h"
+#include "../common/meminfo_lib.h"
 
 #include "ext/standard/info.h"
 #include "ext/standard/php_string.h"
@@ -514,54 +515,6 @@ zend_ulong meminfo_get_element_size(zval *zv)
     }
 
     return size;
-}
-
-/**
- * Escape the \ and " characters for JSON encoding
- */
-char * meminfo_escape_for_json(const char *s)
-{
-    int new_str_len;
-    char *s1;
-
-    s1 = php_str_to_str(s, strlen(s), "\\", 1, "\\\\", 2, &new_str_len);
-
-    return  php_str_to_str(s1, strlen(s1), "\"", 1, "\\\"", 2, &new_str_len);
-}
-
-/**
- * Generate a JSON header for the meminfo
- *
- */
-char * meminfo_info_dump_header(char * header, int header_len)
-{
-    size_t memory_usage;
-    size_t memory_usage_real;
-    size_t peak_memory_usage;
-    size_t peak_memory_usage_real;
-
-    memory_usage = zend_memory_usage(0);
-    memory_usage_real = zend_memory_usage(1);
-
-    peak_memory_usage = zend_memory_peak_usage(0);
-    peak_memory_usage_real = zend_memory_peak_usage(1);
-
-    snprintf(
-        header,
-        header_len,
-        "{\n\
-            \"memory_usage\":%d,\n\
-            \"memory_usage_real\":%d,\n\
-            \"peak_memory_usage\":%d,\n\
-            \"peak_memory_usage_real\":%d\n\
-        }",
-        memory_usage,
-        memory_usage_real,
-        peak_memory_usage,
-        peak_memory_usage_real
-    );
-
-    return header;
 }
 
 /**
